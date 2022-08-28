7675d7db641aa92792c8c5a8de24c1d50a80b733
Bernardo A V <profesorbernardov@gmail.com>
2022-08-28 12:52:05 -0300

    DTO
    DTO

---
+++ src/main/java/com/portfolio/bav/Dto/dtoProyectos.java
@@ -0,0 +1,54 @@
+package com.portfolio.bav.Dto;
+
+import javax.validation.constraints.NotBlank;
+
+
+
+public class dtoProyectos {
+    
+    @NotBlank
+    private String nombrePro;
+    @NotBlank
+    private String descripcionPro;
+    @NotBlank
+    private String fechaPro;
+    
+    //CONSTRUCTORES
+
+    public dtoProyectos() {
+    }
+
+    public dtoProyectos(String nombrePro, String descripcionPro, String fechaPro) {
+        this.nombrePro = nombrePro;
+        this.descripcionPro = descripcionPro;
+        this.fechaPro = fechaPro;
+    }
+    
+    //GETTERS & SETTERS
+
+    public String getNombrePro() {
+        return nombrePro;
+    }
+
+    public void setNombrePro(String nombrePro) {
+        this.nombrePro = nombrePro;
+    }
+
+    public String getDescripcionPro() {
+        return descripcionPro;
+    }
+
+    public void setDescripcionPro(String descripcionPro) {
+        this.descripcionPro = descripcionPro;
+    }
+
+    public String getFechaPro() {
+        return fechaPro;
+    }
+
+    public void setFechaPro(String fechaPro) {
+        this.fechaPro = fechaPro;
+    }
+    
+    
+}
