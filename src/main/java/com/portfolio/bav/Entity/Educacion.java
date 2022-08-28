04e5f2a0b60d7b611eb1fcfd4f834ad19c956080
Bernardo A V <profesorbernardov@gmail.com>
2022-08-28 11:18:16 -0300

    Educacion Entity
    Educacion Entity

---
+++ src/main/java/com/portfolio/bav/Entity/Educacion.java
@@ -0,0 +1,64 @@
+package com.portfolio.bav.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Educacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombreEdu;
+    private String descripcionEdu;
+    private String fechaEdu;
+    
+    //CONSTRUCTORES
+
+    public Educacion() {
+    }
+
+    public Educacion(String nombreEdu, String descripcionEdu, String fechaEdu) {
+        this.nombreEdu = nombreEdu;
+        this.descripcionEdu = descripcionEdu;
+        this.fechaEdu = fechaEdu;
+    }
+    
+    //GETTERS & SETTERS
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreEdu() {
+        return nombreEdu;
+    }
+
+    public void setNombreEdu(String nombreEdu) {
+        this.nombreEdu = nombreEdu;
+    }
+
+    public String getDescripcionEdu() {
+        return descripcionEdu;
+    }
+
+    public void setDescripcionEdu(String descripcionEdu) {
+        this.descripcionEdu = descripcionEdu;
+    }
+
+    public String getFechaEdu() {
+        return fechaEdu;
+    }
+
+    public void setFechaEdu(String fechaEdu) {
+        this.fechaEdu = fechaEdu;
+    }
+    
+    
+        
+}
